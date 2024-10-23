fa628ff3b44d125b0115e1bcba0ad0a50e9170c3
Roshan <richardroshan0110@gmail.com>
2024-10-23 08:32:32 +0100

    Add starter code files

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// let score = 20;
+// let highScore = 0;
+// // let secretNo = math.trunc(math.random * 20) + 1;
+
+// document.querySelector('.check').addEventListener('click', function () {
+//   console.log('working...');
+//   console.log(document.querySelector('.guess').value);
+// });
